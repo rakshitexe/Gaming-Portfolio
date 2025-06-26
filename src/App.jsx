f18fe3b78e810302e05d4394cbd8f42e7742e67f
rakshitexe <rakshit.cse@gmail.com>
2025-06-26 22:39:53 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Hero from './components/Hero'
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'><Hero/></main>
-  )
-}
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
 
-export default App
+      <Hero />
+      <About/>
+    </main>
+  );
+};
+
+export default App;
